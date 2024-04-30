fa9e4e54dcfb9c4c63c9626f5eb7f057e9fc66f9
ste <mrbarrow@hotmail.co.uk>
2024-04-30 14:53:31 +0100

    ex setup

---
+++ java-junit/src/test/java/WordAnalyserTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordAnalyserTest {
+
+    @Test
+    void findLongestWords() {
+    }
+
+    @Test
+    void calculateLetterFrequency() {
+    }
+}
