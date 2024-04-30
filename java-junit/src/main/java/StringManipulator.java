f0e13850087bc6ed77c3e50d2ae0764321eda6ab
ste <mrbarrow@hotmail.co.uk>
2024-04-30 14:51:26 +0100

    feat: ex7

--- java-junit/src/main/java/StringManipulator.java
+++ java-junit/src/main/java/StringManipulator.java
@@ -12,10 +12,10 @@ public class StringManipulator {
         // TODO: Implement the logic to check if the input string is a palindrome
         // (A palindrome = same forwards as backwards)
         for(int i=0;i<input.length()/2;i++){
-            if(input.charAt(i) == input.charAt(input.length()-i)){
-
-               // return true;
+            if(input.charAt(i) != input.charAt(input.length()-i-1)){
+                  return false;
             }
-        } return false;
+        }
+        return true;
     }
 }
