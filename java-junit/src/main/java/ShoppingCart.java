4d40131477579ccbd68341478695af01d656d0fc
ste <mrbarrow@hotmail.co.uk>
2024-04-30 16:54:51 +0100

    feat: ex 9, created Shopping cart + initial tests

--- java-junit/src/main/java/ShoppingCart.java
+++ java-junit/src/main/java/ShoppingCart.java
@@ -4,11 +4,16 @@ import java.util.Map;
 public class ShoppingCart {
 
     private Map<String, Double> items = new HashMap<>();
-    public void addItems(){
 
+    public void addItems(String item, double price){
+        this.items.put(item, price);
     }
+
     public double calculateTotal(){
-
+        return 0.0;
     }
 
+    public Map<String, Double> getItems() {
+        return this.items;
+    }
 }
--- java-junit/src/test/java/ShoppingCartTest.java
+++ java-junit/src/test/java/ShoppingCartTest.java
@@ -6,9 +6,27 @@ class ShoppingCartTest {
 
     @Test
     void addItems() {
+        //Assign
+        String item = "apple";
+        double price = 0.5;
+        ShoppingCart sc = new ShoppingCart();
+
+        //Act
+        sc.addItems(item, price);
+        sc.addItems("hot dogs", 1.0);
+        sc.addItems("beans", 1.5);
+
+        //Assert
+        assertEquals(3, sc.getItems().size());
+        assertEquals(0.5, sc.getItems().get("apple"));
     }
 
     @Test
     void calculateTotal() {
+        //Assign
+
+        //Act
+
+        //Assert
     }
 }
