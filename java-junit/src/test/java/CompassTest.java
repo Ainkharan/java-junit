5b8a9704329979f631ed2a0b99002d103e14f349
nada <ainkharansoftdev@gmail.com>
2024-04-30 11:26:38 +0100

    Feat :Turning compass added

--- java-junit/src/test/java/CompassTest.java
+++ java-junit/src/test/java/CompassTest.java
@@ -8,5 +8,13 @@ class CompassTest {
     @DisplayName("correctly rotates a compass point in a specified direction")
     void rotate() {
         assertEquals(Compass.Point.EAST, Compass.rotate(Compass.Point.NORTH, Compass.Direction.RIGHT));
-    }
+
+        assertAll(() -> assertEquals(Compass.Point.EAST, Compass.rotate(Compass.Point.NORTH, Compass.Direction.RIGHT)),
+                assertEquals(Compass.Point.SOUTH, Compass.rotate(Compass.Point.EAST, Compass.Direction.RIGHT)),
+                assertEquals(Compass.Point.WEST, Compass.rotate(Compass.Point.SOUTH, Compass.Direction.RIGHT))
+
+
+
+                        );
+            }
 }
