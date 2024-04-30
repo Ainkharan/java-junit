5ff09304a7b00faa5adb2d9f3ffe40dadea66286
nada <ainkharansoftdev@gmail.com>
2024-04-30 11:52:45 +0100

    Feat: Challenge 6 rotate methode added.

--- java-junit/src/main/java/Compass.java
+++ java-junit/src/main/java/Compass.java
@@ -13,7 +13,26 @@ public class Compass {
 
     private Point point;
 
-    public static Point rotate(Point point, Direction direction){
-        return Point.EAST;
+    public static Point rotate(Point point, Direction direction) {
+        if ((point == Point.NORTH) && (direction == Direction.RIGHT)) {
+            return Point.EAST;
+        } else if (point == Point.EAST && direction == Direction.RIGHT) {
+            return Point.SOUTH;
+        } else if (point == Point.SOUTH && direction == Direction.RIGHT) {
+            return Point.WEST;
+        } else if (point == Point.WEST && direction == Direction.RIGHT) {
+            return Point.NORTH;
+        }// Left
+        else if (point == Point.NORTH && direction == Direction.LEFT) {
+            return Point.WEST;
+        } else if (point == Point.EAST && direction == Direction.LEFT) {
+            return Point.NORTH;
+        } else if (point == Point.SOUTH && direction == Direction.LEFT) {
+            return Point.EAST;
+        } else if (point == Point.WEST && direction == Direction.LEFT) {
+            return Point.SOUTH;
+        }
+
+        return null;
     }
 }
